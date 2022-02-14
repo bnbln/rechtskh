86ed4bab9b5134b26fb4af639b7a3b11c4a28faa
bnbln <benediktschnupp@me.com>
2022-02-14 15:31:23 +0100

    hero

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, {useState, useEffect} from "react";
+import PropTypes, { func } from "prop-types";
 import { Link, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { Navbar, Container, Nav, NavDropdown, Button, Row, Col, Card, Image} from 'react-bootstrap';
@@ -70,6 +70,7 @@ function Banner() {
   )
 }
 
+
 // eslint-disable-next-line
 export const IndexPageTemplate = ({
   images,
@@ -78,26 +79,59 @@ export const IndexPageTemplate = ({
   ctatext,
   ctalink
 }) => {
+  const [heroState, setHeroState] = useState(0)
+  const [hoverState, setHoverState] = useState(false)
   const heroImage = getImage(images[0].image);
   const heroImage1 = getImage(images[1].image);
+  console.log(hoverState);
+
+  useEffect(() => {
+    if(hoverState == false) {
+    const intervalId = setInterval(() => {
+      console.log("loop");
+      
+        if (heroState === 0) {
+          setHeroState(1)
+        } else {
+          setHeroState(0)
+        }
+    }, 6000);
+    return () => clearInterval(intervalId);
+  }
+  }, [hoverState, heroState]);
+  
+
+  function setToggle(input) {
+    console.log("Stop Loop");
+    setHeroState(input);
+    setHoverState(true)
+    
+    
+  }
+  var timingfunction = "3s cubic-bezier(0.45, 0, 0.55, 1)"
   return (
     <>
       <Container>
       <Row className="justify-content-between align-items-md-center herorow">
+
         <Col md={12} lg={4} xl={3}>
           <h1>{title}</h1>
           <p>{lead}</p>
           <Button variant="secondary" to={ctalink}>{ctatext}</Button>
         </Col>
+
         <Col md={12} lg={8} xl={9}>
-          <Row className='d-flex justify-content-end'>
-            <Col xs={10} md={10} lg={8}>
+          <Row className='d-flex justify-content-end align-items-center'>
+
+            <Col xs={heroState === 0 ? 10 : 2} md={heroState === 0 ? 10 : 2} lg={heroState === 0 ? 8 : 2} style={{height: "60vh", transition: timingfunction}} onMouseOut={()=>setHoverState(false)} onMouseOver={()=>setToggle(0)} >
               <Link to={images[0].link}>
               <div style={{
-                  position: "relative"
+                  position: "relative",
+                  overflow: "hidden"
                 }}>
                   <h1 style={{
                     position: "absolute",
+                    maxWidth: 100,
                     left: 5,
                     bottom: 0,
                     zIndex: 10,
@@ -105,23 +139,57 @@ export const IndexPageTemplate = ({
                     marginBottom: "2rem",
                     color: "white",
                     fontWeight: 700,
-                    lineHeight: 1
+                    lineHeight: 1,
+                    transition: timingfunction,
+                    opacity: heroState === 0 ? 1 : 0,
+                    transform: heroState === 0 ? "translateX(0px)" : "translateX(-370px)"
                   }}>{images[0].title}</h1>
-                  <GatsbyImage image={heroImage} alt={images[0].title} />                
+                  <div style={{
+                    height: heroState === 0 ? "70vh" : "60vh",
+                    transition: timingfunction,
+                    position: "relative",
+                    marginTop: heroState === 0 ? "0vh" : "5vh"
+                    }}>
+                      <GatsbyImage image={heroImage || images[0].path} alt={images[0].title} style={{height: "100%"}}  /> 
+                  </div>
+                                 
                 </div>
               </Link>
             </Col>
-            <Col xs={2} md={2} lg={2} className='window02'>
+            
+            <Col xs={heroState === 0 ? 2 : 10} md={heroState === 0 ? 2 : 10} lg={heroState === 0 ? 2 : 8} className='window02' style={{height: "60vh", transition: timingfunction}} onMouseOut={()=>setHoverState(false)} onMouseOver={()=>setToggle(1)} >
               <Link to={images[1].link}>
-              <div>
-            <GatsbyImage image={heroImage1} alt={images[1].title} />  
-              <p style={{
-                lineHeight: 1,
-                marginTop: "1rem"
-              }}>{images[1].title}</p>
-              </div>
+              <div style={{
+                  position: "relative",
+                  overflow: "hidden"
+                }}>
+                  <h1 style={{
+                    position: "absolute",
+                    maxWidth: 100,
+                    left: 5,
+                    bottom: 0,
+                    zIndex: 10,
+                    marginLeft: "2rem",
+                    marginBottom: "2rem",
+                    color: "white",
+                    fontWeight: 700,
+                    lineHeight: 1,
+                    transition: timingfunction,
+                    opacity: heroState === 1 ? 1 : 0,
+                    transform: heroState === 1 ? "translateX(0px)" : "translateX(-370px)"
+                  }}>{images[1].title}</h1>
+                  <div style={{
+                    height: heroState === 1 ? "70vh" : "60vh",
+                    transition: timingfunction,
+                    position: "relative",
+                    marginTop: heroState === 1 ? "0vh" : "5vh"
+                    }}>
+                   <GatsbyImage image={heroImage1 || images[1].path} alt={images[1].title} style={{height:"100%"}} /> 
+                  </div>               
+                </div>
               </Link>
             </Col>
+
           </Row>
         </Col>
       </Row>
