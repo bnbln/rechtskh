689970b65976bcbf1d0f2ed931e942b1ee7bacb6
bnbln <benediktschnupp@me.com>
2022-11-13 22:10:27 +0100

    Editor

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Rechtsklarheit.de",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      " Rechtsanwalt Tarik Sharief - Kanzlei am Berliner Wittenbergplatz. Ihr Partner für Versicherungsrecht, Verkehrsrecht und Mietrecht",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
