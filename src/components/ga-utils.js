88a04f9a7be2541749a32728c69e62d25977d54e
bnbln <benediktschnupp@me.com>
2022-12-05 16:10:36 +0100

    ga

--- src/components/ga-utils.js
+++ src/components/ga-utils.js
@@ -1,7 +1,8 @@
 import * as ReactGA from "react-ga";
 
 export const initGA = (id) => {
-    if (process.env.NODE_ENV === "production") {
+   // if (process.env.NODE_ENV === "production") {
+        console.log("init ga");
       ReactGA.initialize(id);
-    }
+   // }
   };
